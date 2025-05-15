c082512b776499c1f777133de936b917efa73805
anunes <alef.silva@hbsa.com.br>
2025-05-15 11:15:02 -0300

    feat: implement https://openid.net/specs/openid-connect-backchannel-1_0.html

--- src/MyAppWithMud.Web/SessionManagement/OidcEvents.cs
+++ src/MyAppWithMud.Web/SessionManagement/OidcEvents.cs
@@ -11,12 +11,14 @@ public class OidcEvents : OpenIdConnectEvents
     private readonly IUserTokenStore _store;
     private readonly IUserAccessor _userAccessor;
     private readonly IUserTokenManagementService _service;
+    private readonly ILogger _logger;
 
-    public OidcEvents(IUserTokenStore store, IUserAccessor userAccessor, IUserTokenManagementService service)
+    public OidcEvents(IUserTokenStore store, IUserAccessor userAccessor, IUserTokenManagementService service, ILogger<OidcEvents> logger)
     {
         _store = store;
         _userAccessor = userAccessor;
         _service = service;
+        _logger = logger;
     }
 
     public override Task RedirectToIdentityProvider(RedirectContext context)
@@ -27,22 +29,76 @@ public class OidcEvents : OpenIdConnectEvents
 
     public override async Task RemoteSignOut(RemoteSignOutContext context)
     {
-        var token = context.ProtocolMessage!.Parameters["logout_token"];
-        var validationParameters = context.Options.TokenValidationParameters.Clone();
-        var _configuration = await context.Options.ConfigurationManager!.GetConfigurationAsync(context.HttpContext.RequestAborted);
+        if (context.ProtocolMessage is null)
+        {
+            _logger.LogError("ProtocolMessage must not be null");
+            HandleBadRequest(context);
+            return;
+        }
 
-        var issuer = new[] { _configuration.Issuer };
+        if (!context.ProtocolMessage.Parameters.TryGetValue("logout_token", out var token))
+        {
+            _logger.LogError("logout_token must not be null");
+            HandleBadRequest(context);
+            return;
+        }
+
+        var validationParameters = context.Options.TokenValidationParameters.Clone();
+        var configuration = await context.Options.ConfigurationManager!.GetConfigurationAsync(context.HttpContext.RequestAborted);
+
+        var issuer = new[] { configuration.Issuer };
         validationParameters.ValidIssuers = validationParameters.ValidIssuers?.Concat(issuer) ?? issuer;
 
-        validationParameters.IssuerSigningKeys = validationParameters.IssuerSigningKeys?.Concat(_configuration.SigningKeys)
-            ?? _configuration.SigningKeys;
+        validationParameters.IssuerSigningKeys = validationParameters.IssuerSigningKeys?.Concat(configuration.SigningKeys)
+            ?? configuration.SigningKeys;
 
         var tokenValidationResult = await context.Options.TokenHandler.ValidateTokenAsync(token, validationParameters);
-        var user = new ClaimsPrincipal(tokenValidationResult.ClaimsIdentity);
-        //var jwt = JwtSecurityTokenConverter.Convert(tokenValidationResult.SecurityToken as JsonWebToken);
+        if (!tokenValidationResult.IsValid)
+        {
+            _logger.LogError(tokenValidationResult.Exception, "erro on validating token");
+            HandleBadRequest(context);
+            return;
+        }
 
-        await _service.RevokeRefreshTokenAsync(user);
+        var principal = new ClaimsPrincipal(tokenValidationResult.ClaimsIdentity);
+        var events = principal.FindFirst("events")?.Value;
+        if (string.IsNullOrWhiteSpace(events))
+        {
+            _logger.LogError(tokenValidationResult.Exception, "events claim is required");
+            HandleBadRequest(context);
+            return;
+        }
+
+        if (!events.Contains("http://schemas.openid.net/event/backchannel-logout"))
+        {
+            _logger.LogError(tokenValidationResult.Exception, "events must be 'http://schemas.openid.net/event/backchannel-logout'");
+            HandleBadRequest(context);
+            return;
+        }
+
+        var sub = principal.FindFirst("sub")?.Value;
+        var sid = principal.FindFirst("sid")?.Value;
+
+        if (string.IsNullOrEmpty(sub) && string.IsNullOrEmpty(sid))
+        {
+            _logger.LogError("Token must contain 'sub' or 'sid'");
+            HandleBadRequest(context);
+            return;
+        }
+
+        await _service.RevokeRefreshTokenAsync(principal);
+        context.Principal = principal;
         context.Success();
+
+        static void HandleBadRequest(RemoteSignOutContext context)
+        {
+            context.HandleResponse();
+            context.Response.StatusCode = 400;
+
+            // Emit Cache-Control=no-cache to prevent client caching.
+            context.Response.Headers.CacheControl = "no-cache, no-store";
+            context.Response.Headers.Pragma = "no-cache";
+        }
     }
 
     public override async Task TokenValidated(TokenValidatedContext context)
